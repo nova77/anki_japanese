13dbfc0e04f964d90397479838bab93a570f4694
Norman <nova77@gmail.com>
2021-05-23 17:21:50 +0100

    working version

--- anki.py
+++ anki.py
@@ -5,46 +5,89 @@ import genanki
 from typing import Iterator
 from cards_io import CardsTuples
 
+# CARD_STYLE = """
+# .card {
+#   font-family: arial;
+#   font-size: 30px;
+#   text-align: center;
+#   color: black;
+#   background-color: white;
+# }
+# """
+
 CARD_STYLE = """
-  .card {
-    font-family: arial;
-    font-size: 30px;
-    text-align: center;
-    color: black;
-    background-color: white;
-  }
-  .from {
-    font-style: italic;
-  }
+.front {
+  background-color: white;
+  font-family: arial;
+  padding: 5px;
+  border-radius: 30px;
+}
+
+.back {
+  background-color: #caf2fe;
+  font-family: arial;
+  padding: 5px;
+  border-radius: 30px;
+}
+
+.text {
+  font-family: arial;
+  font-size: 30px;
+  color: black;
+  text-align: center;
+}
 """
+
+FRONT_TEMPLATE = """
+<div class=front>
+<div class=text>
+{{English}}
+</div>
+</div>
+"""
+
+BACK_TEMPLATE = """
+{{FrontSide}}
+<hr id="answer">
+<div class=back>
+<div class=text>
+{{Japanese}}<br/>({{Romanji}})
+</div>
+</div>
+"""
+
 DECK_ID = 8899424244
 
 
 def get_notes(model: genanki.Model, cards: CardsTuples) -> Iterator[genanki.Note]:
-  for front, back in cards:
-    yield genanki.Note(model,
-                       fields=[front, back])
+  for fields in cards:
+    yield genanki.Note(model, fields=fields)
 
 
-def get_deck(id: int, name: str, model: genanki.Model, cards: CardsTuples) -> genanki.Deck:
-  deck = genanki.Deck(id, name)
+def get_deck(deck_id: int, name: str, model: genanki.Model, cards: CardsTuples) -> genanki.Deck:
+  deck = genanki.Deck(deck_id, name)
   for note in get_notes(model, cards):
     deck.add_note(note)
   return deck
 
 
-def get_model(id: int, name: str):
+def get_model(model_id: int, name: str):
   fields = [
       {'name': 'English'},
-      {'name': 'Japanese'}
+      {'name': 'Japanese'},
+      {'name': 'Romanji'},
   ]
   templates = [
       {
-          'name': 'Card 1',
-          'qfmt': '{{English}}',
-          'afmt': '{{FrontSide}}<hr id="answer">{{Japanese}}',
+          'name': 'en/ja',
+          'qfmt': FRONT_TEMPLATE,
+          'afmt': BACK_TEMPLATE
       }
   ]
 
-  return genanki.Model(id, name, fields=fields,
+  return genanki.Model(model_id, name, fields=fields,
                        templates=templates, css=CARD_STYLE)
+
+
+def write_deck(deck: genanki.Deck, path: str):
+  genanki.Package(deck).write_to_file(path)  # 'theogony_vocab.apkg'
--- main.py
+++ main.py
@@ -1,21 +1,38 @@
+import os
 
 from absl import logging
 from absl import flags
 from absl import app
 
-from . import cards_io
+import cards_io
+import anki
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('cards_dir', 'cards', 'The path to the cards')
-flags.DEFINE_string('output', 'collection.apkg', 'The output collection')
+cards_dir = flags.DEFINE_string('cards_dir', 'cards', 'The path to the cards')
+output_dir = flags.DEFINE_string(
+    'output_dir', 'decks', 'The output dir where to write the decks')
 
+model_id = flags.DEFINE_integer('model_id', 12345, 'The model id')
+model_name = flags.DEFINE_string(
+    'model_name', 'Japanese Decks', 'The model name')
 
 
 def main(argv):
   del argv  # Unused.
+  cards = cards_io.load_cards(cards_dir.value)
+  logging.info(f'Loaded {len(cards)} cards.')
+  model = anki.get_model(model_id.value, model_name.value)
 
+  for name, cards in cards.items():
+    deck_id = hash(name)
+    deck = anki.get_deck(deck_id, name, model, cards)
 
+    output_fname = os.path.join(output_dir.value, f'{name}.apkg')
+    anki.write_deck(deck, output_fname)
+    logging.info(f'Wrote {output_fname}')
+
+  logging.info('All done!')
 
 
 if __name__ == '__main__':
